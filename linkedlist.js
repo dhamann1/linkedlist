6575e023ed2ce0560efefd33a8673dd1fdceb5c5
Dean Hamann <hamann.dean@gmail.com>
2019-01-16 23:01:29 -0800

    Adds addToTail

--- linkedlist.js
+++ linkedlist.js
@@ -1,25 +1,30 @@
-function LinkedList () {
+function LinkedList() {
   this.head = null;
   this.tail = null;
 }
 
-function Node(value, next, prev){
+function Node(value, next, prev) {
   this.value = value;
   this.next = next;
   this.prev = prev;
 }
 
 LinkedList.prototype.addToHead = function (value) {
-  var newNode = new Node (value, this.head, null);
+  var newNode = new Node(value, this.head, null);
   if (this.head) {
     this.head.prev = newNode;
-  }
-  else {
+  } else {
     this.tail = newNode;
   }
-  this.head = newNode; 
+  this.head = newNode;
 }
 
-var ll = new LinkedList();
-
-ll.addToHead(100);
+LinkedList.prototype.addToTail = function (value) {
+  var newNode = new Node(value, null, this.tail);
+  if (this.tail) {
+    this.tail.next = newNode;
+  } else {
+    this.head = newNode;
+  }
+  this.tail = newNode
+}
