ebf547819ce77ccc2d76e5252ddd6423da597584
Dean <hamann.dean@gmail.com>
2019-01-18 21:07:37 -0800

    Adds removeTail

--- linkedlist.js
+++ linkedlist.js
@@ -38,7 +38,21 @@ LinkedList.prototype.removeHead = function() {
   if (this.head) {
     this.head.prev = null;
   } else {
-      this.tail = null;
+    this.tail = null;
+  }
+  return val;
+}
+
+LinkedList.prototype.removeTail = function() {
+  if (!this.tail) {
+    return null;
+  }
+  var val = this.tail.value;
+  this.tail = this.tail.prev;
+  if (this.tail) {
+    this.tail.nextd = null;
+  } else {
+    this.head = null;
   }
   return val;
 }
